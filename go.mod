80086af63904e92b632f3b165de65c04433e53a9
pinzolo <pinzolo@gmail.com>
2019-10-02 17:14:13 +0900

    First commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/pinzolo/vw
+
+go 1.12
+
+require github.com/shopspring/decimal v0.0.0-20190905144223-a36b5d85f337
---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/shopspring/decimal"
+)
+
+var base int
+
+func main() {
+	flag.IntVar(&base, "base", 375, "base width(px)")
+	flag.Parse()
+
+	if len(os.Args) == 1 {
+		fmt.Fprintln(os.Stderr, "value required")
+		os.Exit(1)
+	}
+
+	v, err := decimal.NewFromString(os.Args[1])
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	fmt.Println(v.Mul(decimal.New(100, 0)).Div(decimal.New(int64(base), 0)))
+}
